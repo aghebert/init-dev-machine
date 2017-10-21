017fa522b8048b23b3ff36d6a856d04bae496e0a
Ajari Winfield <ajari.winfield@gmail.com>
2017-10-21 16:03:54 -0400

    bypassed some of the prompts for unattended install... needs work

--- initialize.sh
+++ initialize.sh
@@ -5,12 +5,12 @@
 # Elementary OS machine
 
 ##### Upgrade #########################
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update 
+sudo apt upgrade -y
 
 ##### Behavior #########################
 # Enable PPA's
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 
 # Change applications menu hotkey to windows key
 gsettings set org.gnome.mutter overlay-key "'Super_L'"
@@ -19,10 +19,10 @@ gsettings set org.pantheon.desktop.gala.behavior overlay-action "'wingpanel --to
 ##### Aesthetic #########################
 # Install Elementary Tweaks:
 sudo add-apt-repository ppa:philip.scott/elementary-tweaks && sudo apt-get update
-sudo apt-get install elementary-tweaks
+sudo apt install -y elementary-tweaks
 
 # Install OSX-ARC Themes
-sudo apt-get install gdebi
+sudo apt install -y gdebi
 mkdir ~/temp
 cd ~/temp
 sudo wget https://github.com/LinxGem33/OSX-Arc-Darker/releases/download/v1.4.3/osx-arc-collection_1.4.3_amd64.deb
@@ -31,18 +31,18 @@ cd rm -rf ~/temp
 
 # Icons
 sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt-get install papirus-icon-theme
+sudo apt update
+sudo apt install -y papirus-icon-theme
 
 ##### Dev Env #########################
 # Make gnome-terminal default
-sudo apt-get install gnome-terminal
+sudo apt install -y gnome-terminal
 sudo sed -i -e 's/OnlyShowIn=GNOME;Unity;/#OnlyShowIn=Gnome;Unity;/g' /usr/share/applications/gnome-terminal.desktop
 sudo sed -i -e 's/OnlyShowIn=Unity/#OnlyShowIn=Unity/g' /usr/share/applications/gnome-terminal.desktop
 gsettings set org.gnome.desktop.default-applications.terminal exec gnome-terminal
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
-sudo apt-get purge pantheon-terminal
-sudo apt-get install dconf-cli
+sudo apt purge pantheon-terminal
+sudo apt install -y dconf-cli
 wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
 
 # install atom text editor
@@ -53,12 +53,12 @@ sudo gdebi atom-amd64.deb
 cd rm -rf ~/temp
 
 # install chromium browser
-sudo apt-get install chromium-browser
+sudo apt install -y chromium-browser
 
 # install node + npm
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt install -y nodejs
+sudo apt install -y npm
 
 # install virtualvbox + vagrant
-sudo apt-get install virtualbox
-sudo apt-get install vagrant
+sudo apt install -y virtualbox
+sudo apt install -y vagrant
